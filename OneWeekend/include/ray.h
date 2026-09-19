a745f4446d59b2f3bb6a5d488571252dc893d1c8
abhi <abhijeetkc01@gmail.com>
2026-09-19 23:03:14 +0545

    ray class made

--- OneWeekend/include/ray.h
+++ OneWeekend/include/ray.h
@@ -1,4 +1,22 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include "vec3.h"
+
+class ray{
+    public:
+        ray() {}
+        ray(const point3& origin, const vec3& direction) : orig(origin), direc(direction) {}
+        
+        const point3& origin() const {return orig;}
+        const vec3& direction() const {return direc;}
+
+        point3 at(double t){
+            return orig + t*direc;
+        }
+    
+    private:
+        point3 orig;
+        vec3 direc;
+};
 #endif
